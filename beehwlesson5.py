aea0e14fa72fc171829ba6bcab89d393e455a082
sruthy047 <79702452+sruthy047@users.noreply.github.com>
2024-12-24 23:35:57 +0530

    Add files via upload

---
+++ beehwlesson5.py
@@ -0,0 +1,54 @@
+import pygame
+from pygame.locals import*
+from time import*
+pygame.init()
+screen=pygame.display.set_mode((600,600))
+player_x=200
+player_y=200
+keys=[False,False,False,False]
+player=pygame.image.load(r"E:\Sruthi\Python Game Developer II\Lesson 5\bee.png")
+background=pygame.image.load(r"E:\Sruthi\Python Game Developer II\Lesson 5\backgroundhw.png")
+while player_y<600:
+    screen.blit(background,(0,0))
+    screen.blit(player,(player_x,player_y))
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            exit(0)
+        if event.type==pygame.KEYDOWN:
+            if event.key==K_UP:
+                keys[0]=True
+            elif event.key==K_LEFT:
+                keys[1]=True
+            elif event.key==K_DOWN:
+                keys[2]=True
+            elif event.key==K_RIGHT:
+                keys[3]=True
+            
+        if event.type == pygame.KEYUP:
+            #check which button is released
+            if event.key==pygame.K_UP:
+                keys[0]=False
+            elif event.key==pygame.K_LEFT:
+                keys[1]=False
+            elif event.key==pygame.K_DOWN:
+                keys[2]=False
+            elif event.key==pygame.K_RIGHT:
+                keys[3]=False
+    if keys[0]:
+        if player_y>0:
+            player_y-=7
+    elif keys[2]:
+        if player_y<536:
+            player_y+=7
+    if keys[1]:
+        if player_x>0:
+            player_x-=2
+    elif keys[3]:
+        if player_x<536:
+            player_x+=2
+    player_y+=2
+    player_x+=0.5
+    sleep(0.05)
+print("Game over")
